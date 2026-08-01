7874d0cee18aab22a0e48c6165f4a4af0891ea16
Baraa <baraaali.baraa22@gmail.com>
2026-08-01 07:57:23 +0300

    migrations and database

---
+++ app/Models/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'product_name',
+        'price',
+        'size',
+        'description',
+        'category_id',
+        'stock',
+        'slug',
+        'gender',
+        'images',
+    ];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function order_items()
+    {
+        return $this->hasMany(Order_item::class);
+    }
+}
